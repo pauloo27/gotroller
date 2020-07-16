45589da6dd0f007d55c1643556cc1b1d21eed10b
Paulo <pauloeduardo2709@gmail.com>
2020-07-16 16:48:57 -0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/Pauloo27/gotroller
+
+go 1.14
+
+require (
+	github.com/Pauloo27/go-mpris v0.0.0-20200716193135-409b2b9e4720
+	github.com/godbus/dbus v4.1.0+incompatible
+)
---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/Pauloo27/go-mpris"
+	"github.com/godbus/dbus"
+)
+
+const pauseIcon = ""
+const playIcon = ""
+const stoppedIcon = ""
+
+func printToPolybar(player *mpris.Player) {
+	icon := stoppedIcon
+	if player == nil {
+		fmt.Printf("%s\n", icon)
+		return
+	}
+
+	status := player.GetPlaybackStatus()
+
+	if status == mpris.PlaybackPlaying {
+		icon = pauseIcon
+	} else if status == mpris.PlaybackPaused {
+		icon = playIcon
+	} else {
+		log.Fatalf("Invalid playback status %s / %s", status, mpris.PlaybackPaused)
+	}
+	fmt.Printf("%s\n", icon)
+}
+
+func main() {
+	conn, err := dbus.SessionBus()
+	if err != nil {
+		panic(err)
+	}
+	names, err := mpris.List(conn)
+	if err != nil {
+		panic(err)
+	}
+	if len(names) == 0 {
+		printToPolybar(nil)
+		return
+	}
+	player := mpris.New(conn, names[0])
+	printToPolybar(player)
+}
